b88160154670b251704e31932d39a29b4145e1a7
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-09-20 07:22:14 +0530

    models added

---
+++ src/Models/Gate.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class Gate extends BaseModel {
+    private GateType gateType;
+    private int gateNumber;
+    private Operator currentOperator;
+    private GateStatus gateStatus;
+}
---
+++ src/Models/Ticket.java
@@ -0,0 +1,61 @@
+package Models;
+
+import java.util.Date;
+
+public class Ticket extends BaseModel {
+    private  String number;
+    private Date entryTime;
+    private Vehicle vehicle;
+    private ParkingSpot assignedSpot;
+
+    public Gate getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public void setGeneratedAt(Gate generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(Date entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public ParkingSpot getAssignedSpot() {
+        return assignedSpot;
+    }
+
+    public void setAssignedSpot(ParkingSpot assignedSpot) {
+        this.assignedSpot = assignedSpot;
+    }
+
+    public Operator getGeneratedBy() {
+        return generatedBy;
+    }
+
+    public void setGeneratedBy(Operator generatedBy) {
+        this.generatedBy = generatedBy;
+    }
+
+    private Gate generatedAt;
+    private Operator generatedBy;
+}
