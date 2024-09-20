41d266ca75772aded62dd13b275eef05867ca10b
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-09-20 07:07:41 +0530

    First Commit

---
+++ src/Controllers/TicketController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import DTOs.IssueTicketResponseDto;
+import Services.TicketService;
+
+public class TicketController {
+
+    private TicketService ticketService;
+
+    public IssueTicketResponseDto issueTicket(IssueTicketResponseDto responseDto) {
+        return null;
+    }
+}
