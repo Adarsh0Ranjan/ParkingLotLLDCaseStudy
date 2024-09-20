6195bb1afc540d430f243f12e53897ed214df02c
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-09-20 08:58:44 +0530

    exception and vehicle repo added

--- src/Models/Gate.java
+++ src/Models/Gate.java
@@ -1,6 +1,38 @@
 package Models;
 
 public class Gate extends BaseModel {
+    public GateType getGateType() {
+        return gateType;
+    }
+
+    public void setGateType(GateType gateType) {
+        this.gateType = gateType;
+    }
+
+    public int getGateNumber() {
+        return gateNumber;
+    }
+
+    public void setGateNumber(int gateNumber) {
+        this.gateNumber = gateNumber;
+    }
+
+    public Operator getCurrentOperator() {
+        return currentOperator;
+    }
+
+    public void setCurrentOperator(Operator currentOperator) {
+        this.currentOperator = currentOperator;
+    }
+
+    public GateStatus getGateStatus() {
+        return gateStatus;
+    }
+
+    public void setGateStatus(GateStatus gateStatus) {
+        this.gateStatus = gateStatus;
+    }
+
     private GateType gateType;
     private int gateNumber;
     private Operator currentOperator;
